763527a7a0e9e9840dc5ea896e233f34cdc9b885
qebasy <@qebasyahmed@gmail.com>
2024-12-06 17:25:30 +0200

    airline

---
+++ src/main/java/com/egyptFlightReservation/Model/Airline.java
@@ -0,0 +1,28 @@
+package com.egyptFlightReservation.Model;
+
+import java.util.ArrayList;
+class airline{
+    //class attributes
+    private String name;
+    private int ID;
+    private String location;
+    private ArrayList<Flight> Fights;
+    //Constructor
+    public airline(String name,int ID,String location){
+        this.name=name;
+        this.ID=ID;
+        this.location=location;
+    }
+    // method to get name
+    public String get_name(){
+        return name;
+    }
+    // method to get ID
+    public int get_ID(){
+        return ID;
+    }
+    // method to get location
+    public String get_location(){
+        return location;
+    }
+}
