4ee85a2e4a92b07bc90b0d6f3cedcc94fb636bb4
AnasElwkel <anshesh039@gmail.com>
2024-12-09 08:28:53 +0200

    add general validation of date (in Tools)

---
+++ src/main/java/Tools/DateValidation.java
@@ -0,0 +1,21 @@
+package Tools;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class DateValidation {
+    public static DateTimeFormatter dateFormatter=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static LocalDate getDate(String msg) {
+        System.out.println(msg + "in format (yyyy-MM-dd): ");
+        String s = Input.cin.nextLine();
+        try{
+            LocalDate ret = LocalDate.parse(s , dateFormatter);
+            return ret;
+        }catch (DateTimeParseException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
