bac940397af03f4521a361c90a563feaaaa33ac8
AnasElwkel <anshesh039@gmail.com>
2024-12-16 22:57:08 +0200

    .

--- src/main/java/TestDrivers/FileSaverTestDriver.java
+++ src/main/java/TestDrivers/FileSaverTestDriver.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 public class FileSaverTestDriver {
     public static void main(String args[]) {
-        String fileName = "/home/anaselwkel/IdeaProjects/Flight_Reservation_Project/src/main/java/TestDrivers/";
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Anas");
-        list.add("Anas nn");
-        list.add("123456789");
-        list.add("123456789");
-
-
-        FileSaver.save(fileName + "folder" + "/Testing", list);
+//        String fileName = "/home/anaselwkel/IdeaProjects/Flight_Reservation_Project/src/main/java/TestDrivers/";
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("Anas");
+//        list.add("Anas nn");
+//        list.add("123456789");
+//        list.add("123456789");
+//
+//
+//        FileSaver.save(fileName + "folder" + "/Testing", list);
 
     }
 }
